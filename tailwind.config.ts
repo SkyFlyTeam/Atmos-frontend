a28077053344f39a6457dc3e4f9528902a1c1322
karengoncalves8 <karengoncalves128@gmail.com>
2025-09-10 08:49:54 -0300

    chore: adicionado template de PR e cores da aplicação

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,20 @@ import type { Config } from "tailwindcss"
 export default {
   darkMode: ["class", "html"],
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
-  theme: { extend: {} },
+  theme: { 
+    extend: {
+      colors: {
+        'dark-green': 'var(--dark-green)',
+        green: 'oklch(0.7259 0.2028 132.47)',
+        'light-green': 'var(--light-green)',
+        'dark-cyan': 'var(--dark-cyan)',
+        'white-pure': 'var(--white-pure)',
+        'white-bg': 'var(--white-bg)',
+        gray: 'var(--gray)',
+        'gray-dark': 'var(--gray-dark)',
+        red: 'var(--red)',
+      },
+    } 
+  },
   plugins: [],
 } satisfies Config
