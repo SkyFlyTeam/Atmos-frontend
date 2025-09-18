a325e2a37e3c3f280e6fa7266514e5dabd9e8a5e
karengoncalves8 <karengoncalves128@gmail.com>
2025-09-17 23:02:54 -0300

    AT-11: adicionando componente side drawer e formulario de cadastro de paramêtro

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog'; // Ajuste os imports conforme a estrutura do seu projeto
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription, DialogFooter } from '@/components/ui/dialog'; 
 
 type ModalProps = {
   open: boolean; // Controle externo do estado aberto/fechado
@@ -12,7 +12,7 @@ type ModalProps = {
 const Modal: React.FC<ModalProps> = ({ open, onClose, title, content, buttons }) => {
   return (
     <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent className='text-base max-w-[calc(100%-2rem)] rounded-md w-fit max-h-[calc(100%-4rem)] h-fit [&>button]:cursor-pointer'>
+      <DialogContent className='fixed top-text-base max-w-[calc(100%-2rem)] rounded-md w-fit max-h-[calc(100%-4rem)] h-fit [&>button]:cursor-pointer [&>button>svg]:w-7 [&>button>svg]:h-7 [&>button>svg]:hover:text-red'>
         <DialogHeader>
           <DialogTitle className='text-2xl'>{title}</DialogTitle>
         </DialogHeader>
