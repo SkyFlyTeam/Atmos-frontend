6a50d0d51ae105285f14b72ffed46133459e98f9
karengoncalves8 <karengoncalves128@gmail.com>
2025-09-17 23:11:00 -0300

    AT-11 fix: ajustado cor do overlay do side drawer

--- src/components/SideDrawer/SideDrawer.tsx
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -10,7 +10,7 @@ type SideDrawerProps = {
 const SideDrawer = ({onClose, title, content}: SideDrawerProps) => {
     return (
         <>
-            <div className="fixed bottom-0 left-0 w-full h-full bg-gray-700/50"></div>
+            <div className="fixed bottom-0 left-0 w-full h-full bg-black/80"></div>
             <div className="fixed bottom-0 right-0 w-fit h-full bg-white-pure p-3 flex flex-col">
                 <div className="w-full">
                     <CgClose className="w-7 h-7 text-gray-400 cursor-pointer hover:text-red" onClick={onClose}/>
