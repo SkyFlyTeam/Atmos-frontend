4cf502fd42b15758d7d1e49aeea0d3ec598a6d54
SarahBatagioti <sarah.montuanibt@gmail.com>
2025-09-21 20:59:42 -0300

    AT-24: adicionado SideDrawer de editar perfil

--- src/components/SideDrawer/SideDrawer.tsx
+++ src/components/SideDrawer/SideDrawer.tsx
@@ -2,27 +2,33 @@ import { ReactNode } from "react"
 import { CgClose } from "react-icons/cg";
 
 type SideDrawerProps = {
-    onClose: () => void,
-    title: string,
-    content: ReactNode
+  onClose: () => void,
+  title: string,
+  content: ReactNode
 }
 
-const SideDrawer = ({onClose, title, content}: SideDrawerProps) => {
-    return (
-        <>
-            <div className="fixed bottom-0 left-0 w-full h-full bg-black/80"></div>
-            <div className="fixed bottom-0 right-0 w-full sm:w-fit h-full bg-white-pure p-3 flex flex-col">
-                <div className="w-full">
-                    <CgClose className="w-7 h-7 text-gray-400 cursor-pointer hover:text-red" onClick={onClose}/>
-                </div>
-                <div className="px-6">
-                    <h1>{title}</h1>
-                    <div className="mt-4">
-                        {content}
-                    </div>
-                </div>
-            </div>
-        </>
+const SideDrawer = ({ onClose, title, content }: SideDrawerProps) => {
+  return (
+    <>
+      <div className="fixed bottom-0 left-0 w-full h-full bg-black/80"></div>
+      <div
+        className="
+          fixed bottom-0 right-0 h-full bg-white-pure p-3 flex flex-col
+          w-full md:w-fit
+        "
+      >
+        <div className="w-full">
+          <CgClose
+            className="w-7 h-7 text-gray-400 cursor-pointer hover:text-red"
+            onClick={onClose}
+          />
+        </div>
+        <div className="px-6">
+          <h1>{title}</h1>
+          <div className="mt-4">{content}</div>
+        </div>
+      </div>
+    </>
   );
 }
 
