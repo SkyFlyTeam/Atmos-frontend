a02faaf7c0006d64b20ded5b15073b7ff0507db6
Guilherme <guilherme.benedito@fatec.sp.gov.br>
2025-09-24 12:04:27 -0300

    AT-25 fix: adicionar mensagens de erros

--- src/interfaces/TipoAlerta.ts
+++ src/interfaces/TipoAlerta.ts
@@ -10,18 +10,8 @@ export interface TipoAlerta {
     pk?: number,
     tipo: string,
     descricao: string,
-<<<<<<< HEAD
     publica: boolean,
     tipo_alarme: number,
     p1: number,
-    p2?: number
-=======
-    criterios: criteriosEnum,
-    valor_referencia: number,
-    segunda_referencia?: number | null,
-    publica?: boolean,
-    tipo_alarme?: number,
-    p1?: number,
     p2?: number | null
->>>>>>> cd44923 (AT-25 feat: adicionar cadastro e edição de tipo-alerta)
 }
