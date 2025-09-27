d1f34a3be9f6c49383cb82fd4bd58a7673c64fd9
Arthur Johannes <arthur.peres@fatec.sp.gov.br>
2025-09-27 10:21:40 -0300

    AT-19 feat: toast com status de acesso

--- src/services/loginServices.ts
+++ src/services/loginServices.ts
@@ -1,12 +1,22 @@
 import { Api } from "@/config/api";
 import { ApiException } from "@/config/apiException";
 import { Login } from "@/interfaces/Login";
+import { AxiosError } from "axios";
 
 const setLogin = async (login: Login): Promise<any | ApiException> => {
     try {
         const { data } = await Api.post("/usuario/login", login)
         return data as Login
     } catch (error) {
+        if (error instanceof AxiosError)
+            switch (error.status) {
+                case 404:
+                case 401:
+                    return new ApiException("ERRO: credenciais inválidas. Verifique email e senha, e tente novamente.\n");
+                default:
+                    return new ApiException("ERRO: resposta sem tratamento, HTTP " + error.status);
+            }
+
         if (error instanceof Error)
             return new ApiException(error.message || "Erro ao realizar login.");
         return new ApiException("Erro desconhecido.");
@@ -14,5 +24,5 @@ const setLogin = async (login: Login): Promise<any | ApiException> => {
 };
 
 export const loginServices = {
-  setLogin
+    setLogin
 };
