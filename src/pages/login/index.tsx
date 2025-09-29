062d33bb8fa34c14cc5155d720d3d4bf665c8706
Eric Lourenço <77221715+ericloumendes@users.noreply.github.com>
2025-09-29 00:25:22 -0300

    fix: AJustando conflitos de merge de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,19 +1,20 @@
 import AtmosIcon from "@/components/AtmosIcon/AtmosIcon";
 import { Button } from "@/components/ui/button";
-import { Form } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { ApiException } from "@/config/apiException";
 import { Login } from "@/interfaces/Login";
+import { Usuario } from "@/interfaces/Usuarios";
 import { londrina } from "@/lib/fonts";
 import { loginServices } from "@/services/loginServices"
-import { error } from "console";
-import { LogIn } from "lucide-react";
+import { useRouter } from "next/router";
 import { ChangeEvent, useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
-
 const LoginPage = () => {
+    const [needLogin, setNeedLogin] = useState<boolean>(false);
+
+    const router = useRouter();
     const blankLogin: Login = {
         email: "",
         senha: "",
@@ -22,6 +23,26 @@ const LoginPage = () => {
     const [loginData, setLoginData] = useState<Login>(blankLogin);
     const [isLoggingIn, setIsLoggingIn] = useState<boolean>(false);
 
+
+    useEffect(() => {
+        const verifyAuth = async () => {
+            setNeedLogin(false)
+            let done = false;
+            try {
+                const auth = await loginServices.getAuth();
+                done = auth;
+            }
+            catch (error) { }
+            finally {
+                if (done)
+                    router.push("/");
+                if (done === undefined)
+                    setNeedLogin(true)
+            }
+        }
+
+        verifyAuth();
+    }, [])
 
     const delayLogin = (ms: number) => new Promise(res => setTimeout(res, ms));
 
@@ -34,14 +55,15 @@ const LoginPage = () => {
         return login;
     }
 
-    const saveLogin = async (token: string) => {
-        localStorage.setItem('token', token);
+    const successLogin = async () => {
         resetFormLogin();
-        toast.success("Sucesso.");
-        // Dispara evento customizado para Navbar atualizar estado
-        if (typeof window !== 'undefined') {
+
+        setNeedLogin(false);
+
+        router.push("/");
+
+        if (typeof window !== 'undefined')
             window.dispatchEvent(new Event('usuarioLogado'));
-        }
     }
 
     const loginLogoAtmos = (sizeRem: number, className?: string) => {
@@ -69,69 +91,77 @@ const LoginPage = () => {
         setIsLoggingIn(true);
 
         const login = await fetchLogin();
+        if ((login as Usuario).email && (login as Usuario).token)
+            successLogin()
+        else
+            toast.error((login as ApiException).message)
 
-        await delayLogin(1200);
 
-        if ((login as Login).email && login.token)
-            saveLogin(login.token)
-        else        
-            toast.error(login.message)
-        
         setIsLoggingIn(false);
     }
 
 
     return (
-        <form onSubmit={handleSubmitLogin}>
-            <div className="absolute text-[#00312D] w-screen min-h-full top-0 left-0 flex flex-row gap-0 z-[-1]">
-                <div className="text-wrap flex flex-col justify-center content-center invisible w-0 md:visible md:w-[50%]" >
-                    {loginLogoAtmos(28)}
-                </div>
-                <div className="bg-[#DEFFD9] align-bottom w-full md:w-[50%] flex flex-col justify-center">
-                    <div className="mx-auto mt-32 mb-10 bg-white rounded-[18] px-8 py-20 md:py-30 min-w-[320px] w-[67%] min-h-[690px] md:min-h-[540px] shadow-[0px_4px_35px_0px_rgba(0,_0,_0,_0.12)] flex flex-col justify-center">
+        <>
+            {needLogin &&
+                <form onSubmit={handleSubmitLogin}>
+                    <div className="absolute text-[#00312D] w-screen min-h-full top-0 left-0 flex flex-row gap-0 z-[-1]">
+                        <div className="text-wrap flex flex-col justify-center content-center invisible w-0 md:visible md:w-[50%]" >
+                            {loginLogoAtmos(28)}
+                        </div>
+                        <div className="bg-[#DEFFD9] align-bottom w-full md:w-[50%] flex flex-col justify-center">
+                            <div className="mx-auto mt-32 mb-10 bg-white rounded-[18] px-8 py-20 md:py-30 min-w-[320px] w-[67%] min-h-[690px] md:min-h-[540px] shadow-[0px_4px_35px_0px_rgba(0,_0,_0,_0.12)] flex flex-col justify-center">
 
-                        {loginLogoAtmos(20, "visible  mb-12 md:invisible md:w-0 md:h-0 md:mb-0")}
+                                {loginLogoAtmos(20, "visible  mb-12 md:invisible md:w-0 md:h-0 md:mb-0")}
 
-                        <div className="flex flex-col justify-center gap-12">
-                            <p className={`text-4xl md:text-5xl ${londrina}`}>Entrar</p>
+                                <div className="flex flex-col justify-center gap-12">
+                                    <p className={`text-4xl md:text-5xl ${londrina}`}>Entrar</p>
 
-                            <div>
-                                <Label htmlFor="loginEmail">Email</Label>
-                                <Input
-                                    className="mt-2 placeholder:text-[#ADADAD]"
-                                    type="text"
-                                    name="loginEmail"
-                                    onChange={handleLoginEmail}
-                                    value={loginData.email}
-                                    placeholder="Email"
-                                    disabled={isLoggingIn}
-                                ></Input>
+                                    <div>
+                                        <Label htmlFor="loginEmail">Email</Label>
+                                        <Input
+                                            className="mt-2 placeholder:text-[#ADADAD]"
+                                            type="text"
+                                            name="loginEmail"
+                                            onChange={handleLoginEmail}
+                                            value={loginData.email}
+                                            placeholder="Email"
+                                            disabled={isLoggingIn}
+                                        ></Input>
+                                    </div>
+
+                                    <div>
+                                        <Label htmlFor="loginSenha">Senha</Label>
+                                        <Input
+                                            className="mt-2 placeholder:text-[#ADADAD]"
+                                            type="password"
+                                            name="loginSenha"
+                                            onChange={handleLoginSenha}
+                                            value={loginData.senha}
+                                            placeholder="Senha"
+                                            disabled={isLoggingIn}
+                                        ></Input>
+                                    </div>
+
+
+                                    <Button
+                                        className="rounded-[12]"
+                                        type="submit"
+                                        disabled={isLoggingIn}
+                                    >Entrar</Button>
+                                    {/* <Button
+                                        className="rounded-[12]"
+                                        type="button"
+                                        onClick={async () => alert(JSON.stringify(await loginServices.getAuth()))}
+                                        disabled={isLoggingIn}
+                                    >TOKEN</Button> */}
+                                </div>
                             </div>
-
-                            <div>
-                                <Label htmlFor="loginSenha">Senha</Label>
-                                <Input
-                                    className="mt-2 placeholder:text-[#ADADAD]"
-                                    type="password"
-                                    name="loginSenha"
-                                    onChange={handleLoginSenha}
-                                    value={loginData.senha}
-                                    placeholder="Senha"
-                                    disabled={isLoggingIn}
-                                ></Input>
-                            </div>
-
-
-                            <Button
-                                className="rounded-[12]"
-                                type="submit"
-                                disabled={isLoggingIn}
-                            >Entrar</Button>
                         </div>
                     </div>
-                </div>
-            </div>
-        </form>
+                </form>
+            }
+        </>
     )
 }
 
