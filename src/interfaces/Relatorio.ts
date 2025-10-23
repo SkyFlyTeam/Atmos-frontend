984eafc0661cf3d0f4c7a9812670fe44c9248172
Arthur Johannes <arthur.peres@fatec.sp.gov.br>
2025-10-23 15:39:47 -0300

    AT-42 refactor: Interface para relatorio

---
+++ src/interfaces/Relatorio.ts
@@ -0,0 +1,11 @@
+export interface Relatorio {
+    estacao_id: number,
+    Parametros_pk: number,
+    media: number,
+    maximo: number,
+    minimo: number,
+    mes: number,
+    ano: number,
+    param: string,
+    unidade: string,
+}
