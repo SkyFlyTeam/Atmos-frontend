f29f8bb9e5a14f74919b956f8492f91e5c1c0e4f
Arthur Johannes <arthur.peres@fatec.sp.gov.br>
2025-10-23 15:43:21 -0300

    AT-42 feat: adicionado componente card de relatorio

---
+++ src/services/valorCapturadoServices.ts
@@ -0,0 +1,48 @@
+import { EstacaoParametroRelacao } from "@/interfaces/ParametroEstacao";
+import { Api } from "@/config/api";
+import { ApiException } from "@/config/apiException";
+import { Login } from "@/interfaces/Login";
+import { Relatorio } from "@/interfaces/Relatorio";
+import { Usuario } from "@/interfaces/Usuarios";
+import { AxiosError } from "axios";
+import { RelatParam } from "@/interfaces/RelatParam";
+
+
+interface relatData {
+    estacao_id: number,
+    Parametros_pk: number,
+    media: number,
+    maximo: number,
+    minimo: number,
+    mes: number,
+    ano: number,
+    parametro: EstacaoParametroRelacao,
+}
+
+const getRelatorio = async (param?: RelatParam): Promise<Relatorio | ApiException> => {
+    try {
+        const { data } = await Api.get("/valor-capturado/relatorio", {
+            params: param
+        });
+        data.forEach((cell: any) => {
+            cell.param = (cell as relatData).parametro.tipoParametro.nome;
+            cell.unidade = (cell as relatData).parametro.tipoParametro.unidade;
+            delete data['parametro'];
+        });
+        return data as Relatorio
+    } catch (error) {
+        if (error instanceof AxiosError)
+            switch (error.status) {
+                default:
+                    return new ApiException("ERRO: resposta sem tratamento, HTTP " + error.status);
+            }
+
+        if (error instanceof Error)
+            return new ApiException(error.message || "Erro ao realizar login.");
+        return new ApiException("Erro desconhecido.");
+    }
+};
+
+export const ValorCapturadoServices = {
+    getRelatorio
+};
