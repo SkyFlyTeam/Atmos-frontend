0c1155702de90ef0212df1bb465371a0a3b786fc
karen goncalves <karen.goncalves@autaza.com>
2025-11-19 13:48:14 -0300

    AT-68 fix: ajustado a responsividade

--- src/pages/guia-educativo/index.tsx
+++ src/pages/guia-educativo/index.tsx
@@ -14,48 +14,63 @@ const GuiaEducativoPage = () => {
             icon: <img src="/atmos-frog.svg" alt="Atmos Frog" width={28} height={28} />,
             subSections: [
                 { index: 0, title: "O que é", icon: <PiTargetBold size={24} />},
+                { index: 1, title: "Hege", icon: <PiTargetBold size={24} />},
             ]
         },
         {
             title: "Estação",
             icon: <RiBaseStationFill size={24} />,
             subSections: [
-                { index: 1, title: "O que é", icon: <PiTargetBold size={24} />},
-                { index: 2, title: "Finalidade", icon: <PiTargetBold size={24} />},
+                { index: 2, title: "O que é", icon: <PiTargetBold size={24} />},
+                { index: 3, title: "Finalidade", icon: <PiTargetBold size={24} />},
+            ]
+        },
+        {
+            title: "Er",
+            icon: <RiBaseStationFill size={24} />,
+            subSections: [
+                { index: 4, title: "O que é", icon: <PiTargetBold size={24} />},
+                { index: 5, title: "Finalidade", icon: <PiTargetBold size={24} />},
             ]
         },
     ];
 
     const handleChangeItem = (index: number) => {
         setSelectedIndex(index);
-        console.log("Item selecionado:", index);
     }
 
     return (
         <>
-            <Card className="flex gap-3 md:shadow-[0px_4px_35px_0px_rgba(0,0,0,0.12)] md:bg-white bg-white-bg shadow-none min-h-full">
-                <div className="border-inherit bg-green rounded-l-lg px-12 py-18 min-w-fit w-full flex-2">
+            <Card className="flex md:flex-row flex-col gap-3 md:shadow-[0px_4px_35px_0px_rgba(0,0,0,0.12)] md:bg-white bg-white-bg shadow-none min-h-full">
+                <div className="border-inherit bg-green md:rounded-l-lg md:rounded-r-none rounded-md md:px-12 md:py-18 p-2 min-w-fit w-full flex-2">
                     <NavigationMenu items={guia_data} handleChangeItem={handleChangeItem} selectedIndex={selectedIndex} />
                 </div>
-                <div className="px-12 py-18 flex-6">
+                <div className="md:py-18 md:px-12 p-2 flex-6">
                     {selectedIndex === 0 && (
                         <GuideContent
                             title="O que é o Guia Educativo?"
                             description="O Guia Educativo é uma ferramenta interativa desenvolvida para auxiliar os usuários a compreenderem melhor o funcionamento e a finalidade das estações Atmos. Através de uma série de seções informativas, o guia oferece explicações detalhadas, imagens ilustrativas e navegação intuitiva para facilitar o aprendizado."
                             imgUrl="/images/guide-1.png"
-                            onBackClick={() => console.log("Voltar clicado")}
                             onNextClick={() => handleChangeItem(selectedIndex + 1)}
                         />
                     )}
                     {selectedIndex === 1 && (
-                        <div>
-                            <h2 className="text-2xl font-bold mb-4">ALOOOOOOO?</h2>
-                        </div>
+                        <GuideContent
+                            title="Hege?"
+                            description="O Guia Educativo é uma ferramenta interativa desenvolvida para auxiliar os usuários a compreenderem melhor o funcionamento e a finalidade das estações Atmos. Através de uma série de seções informativas, o guia oferece explicações detalhadas, imagens ilustrativas e navegação intuitiva para facilitar o aprendizado."
+                            imgUrl="/images/guide-1.png"
+                            onBackClick={() => handleChangeItem(selectedIndex - 1)}
+                            onNextClick={() => handleChangeItem(selectedIndex + 1)}
+                        />
                     )}
                     {selectedIndex === 2 && (
-                        <div>
-                            <h2 className="text-2xl font-bold mb-4">FUEHFSH?</h2>
-                        </div>
+                       <GuideContent
+                            title="EERERE?"
+                            description="O Guia Educativo é uma ferramenta interativa desenvolvida para auxiliar os usuários a compreenderem melhor o funcionamento e a finalidade das estações Atmos. Através de uma série de seções informativas, o guia oferece explicações detalhadas, imagens ilustrativas e navegação intuitiva para facilitar o aprendizado."
+                            imgUrl="/images/guide-1.png"
+                            onBackClick={() => handleChangeItem(selectedIndex - 1)}
+                            onNextClick={() => handleChangeItem(selectedIndex + 1)}
+                        />
                     )}
                 </div>
             </Card>
